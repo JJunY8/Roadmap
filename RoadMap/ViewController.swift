1fe09850efb05113b015e92416883536b7a97453
nayeo2 <nayeo2@naver.com>
2022-07-31 23:03:27 +0900

    로고까지,나영

--- RoadMap/ViewController.swift
+++ RoadMap/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var ImgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+    
+    
     }
-
-
+    
 }
+
 
